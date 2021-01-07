feb86e612cc651482869c246317be5e653bc7202
asimone9914 <asimone9914@gmail.com>
2021-01-06 22:25:50 -0500

    Two quotes per line. User appended lines support added

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,31 @@
 import random
 
 def primary():
-  #print("Keep it logically awesome.")
-
+  # open the quotes.txt file and
+  # run the read().splitlines() to fetch data
+  # and remove any newlines
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.read().splitlines()
   f.close()
-
+  
+  # set up process to generate a random integer
+  # within the range of the list index, to return that index
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  print(quotes[rnd], quotes[rnd - 1], end = "")
+
+  # ask the user if they would like to append a quote
+  ans = input("\nwould you like to add a quote? (y/n)")
+
+  if ans == "y":
+    text = input("enter your text: ")
+    # open the quotes.txt file but
+    # this time, append a quote
+    f = open("quotes.txt", "a")
+    f.write(text + "\n")
+    f.close()
+  elif ans == "n":
+    print("Okay! Exiting..")
 
 if __name__== "__main__":
   primary()
